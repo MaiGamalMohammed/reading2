b8f03a30ff8eaab45682f2aa93d35577cedce4c2
marbella267 <135547136+marbella267@users.noreply.github.com>
2025-12-07 19:39:43 +0200

    edit the book

--- frontend/assets/js/main_auth.js
+++ frontend/assets/js/main_auth.js
@@ -413,6 +413,9 @@ function editBook(){
     let data =   Object.fromEntries(formData)
     data.user_id = JSON.parse(localStorage.getItem('user')).id
     data.id = id
+    data.pages_read = parseInt(data.pages_read)
+data.pages_total = parseInt(data.pages_total)
+data.publication_year = parseInt(data.publication_year)   
      
     let response = await makePutReq("https://localhost:4000/api/book",data)
     if(response.message=="updated") window.location.href = "./index.html"
